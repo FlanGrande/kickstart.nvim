79ab5824411673d43f85af0f2a031462505f1c45
FlanGrande <francisjgrande@gmail.com>
2023-08-19 01:35:33 +0100

    Clean up after LSP replacement

--- init.lua
+++ init.lua
@@ -93,21 +93,6 @@ require('lazy').setup({
     }
   },
 
-  -- -- NOTE: This is where your plugins related to LSP can be installed.
-  -- --  The configuration is done below. Search for lspconfig to find it below.
-  -- {
-  --   -- LSP Configuration & Plugins
-  --   'neovim/nvim-lspconfig',
-  --   dependencies = {
-  --     -- Automatically install LSPs to stdpath for neovim
-  --     { 'williamboman/mason.nvim', config = true },
-  --     'williamboman/mason-lspconfig.nvim',
-  --
-  --     -- Useful status updates for LSP
-  --     -- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
-  --   },
-  -- },
-
   -- Useful plugin to show you pending keybinds.
   { 'folke/which-key.nvim', opts = {} },
   {
@@ -196,7 +181,11 @@ require('lazy').setup({
     build = ':TSUpdate',
   },
 
+
+
+
   -- Mine
+
   {
     'github/copilot.vim'
   },
@@ -220,6 +209,12 @@ require('lazy').setup({
   {
     'BurntSushi/ripgrep'
   },
+
+
+  -- End Mine
+
+
+
 
   --       These are some example plugins that I've included in the kickstart repository.
   --       Uncomment any of the lines below to enable them.
@@ -271,6 +266,7 @@ vim.opt.nu = true
 vim.o.relativenumber = true
 
 vim.opt.updatetime = 50
+vim.opt.timeoutlen = 300
 
 -- Copied from ThePrimeagen
 -- Move content of line up and down
@@ -297,6 +293,7 @@ vim.g.undotree_SetFocusWhenToggle = 1
 -- Change blankline color
 vim.cmd('hi IndentBlankLineChar guifg=#442211 gui=nocombine')
 
+
 -- End Mine
 
 
@@ -312,9 +309,6 @@ vim.o.hlsearch = false
 -- Make line numbers default
 vim.wo.number = true
 
--- Enable mouse mode
-vim.o.mouse = 'a'
-
 -- Sync clipboard between OS and Neovim.
 --  Remove this option if you want your OS clipboard to remain independent.
 --  See `:help 'clipboard'`
@@ -323,19 +317,12 @@ vim.o.clipboard = 'unnamedplus'
 -- Enable break indent
 vim.o.breakindent = true
 
--- Save undo history
-vim.o.undofile = true
-
 -- Case-insensitive searching UNLESS \C or capital in search
 vim.o.ignorecase = true
 vim.o.smartcase = true
 
 -- Keep signcolumn on by default
 vim.wo.signcolumn = 'yes'
-
--- Decrease update time
-vim.o.updatetime = 250
-vim.o.timeoutlen = 300
 
 -- Set completeopt to have a better completion experience
 vim.o.completeopt = 'menuone,noselect'
@@ -348,10 +335,6 @@ vim.o.termguicolors = true
 -- Keymaps for better default experience
 -- See `:help vim.keymap.set()`
 vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
-
--- Remap for dealing with word wrap
--- vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
--- vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
 
 -- [[ Highlight on yank ]]
 -- See `:help vim.highlight.on_yank()`
@@ -475,102 +458,15 @@ local lsp = require('lsp-zero').preset({})
 lsp.on_attach(function(client, bufnr)
   -- see :help lsp-zero-keybindings
   -- to learn the available actions
-  -- lsp.default_keymaps({buffer = bufnr})
+  lsp.default_keymaps({buffer = bufnr})
 end)
 
 require('lspconfig').lua_ls.setup(lsp.nvim_lua_ls())
 
 lsp.setup()
 
--- -- [[ Configure LSP ]]
--- --  This function gets run when an LSP connects to a particular buffer.
--- local on_attach = function(_, bufnr)
---   -- NOTE: Remember that lua is a real programming language, and as such it is possible
---   -- to define small helper and utility functions so you don't have to repeat yourself
---   -- many times.
---   --
---   -- In this case, we create a function that lets us more easily define mappings specific
---   -- for LSP related items. It sets the mode, buffer and description for us each time.
---   local nmap = function(keys, func, desc)
---     if desc then
---       desc = 'LSP: ' .. desc
---     end
---
---     vim.keymap.set('n', keys, func, { buffer = bufnr, desc = desc })
---   end
---
---   nmap('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
---   nmap('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
---
---   nmap('gd', vim.lsp.buf.definition, '[G]oto [D]efinition')
---   nmap('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
---   nmap('gI', vim.lsp.buf.implementation, '[G]oto [I]mplementation')
---   nmap('<leader>D', vim.lsp.buf.type_definition, 'Type [D]efinition')
---   nmap('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
---   nmap('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
---
---   -- See `:help K` for why this keymap
---   nmap('K', vim.lsp.buf.hover, 'Hover Documentation')
---   nmap('<C-k>', vim.lsp.buf.signature_help, 'Signature Documentation')
---
---   -- Lesser used LSP functionality
---   nmap('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
---   nmap('<leader>wa', vim.lsp.buf.add_workspace_folder, '[W]orkspace [A]dd Folder')
---   nmap('<leader>wr', vim.lsp.buf.remove_workspace_folder, '[W]orkspace [R]emove Folder')
---   nmap('<leader>wl', function()
---     print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
---   end, '[W]orkspace [L]ist Folders')
---
---   -- Create a command `:Format` local to the LSP buffer
---   vim.api.nvim_buf_create_user_command(bufnr, 'Format', function(_)
---     vim.lsp.buf.format()
---   end, { desc = 'Format current buffer with LSP' })
--- end
-
--- Enable the following language servers
---  Feel free to add/remove any LSPs that you want here. They will automatically be installed.
---
---  Add any additional override configuration in the following tables. They will be passed to
---  the `settings` field of the server config. You must look up that documentation yourself.
---
---  If you want to override the default filetypes that your language server will attach to you can
---  define the property 'filetypes' to the map in question.
--- local servers = {
---   -- clangd = {},
---   -- gopls = {},
---   -- pyright = {},
---   -- rust_analyzer = {},
---   -- tsserver = {},
---   -- html = { filetypes = { 'html', 'twig', 'hbs'} },
---
---   lua_ls = {
---     Lua = {
---       workspace = { checkThirdParty = false },
---       telemetry = { enable = false },
---     },
---   },
--- }
-
 -- Setup neovim lua configuration
 require('neodev').setup()
-
--- Ensure the servers above are installed
--- local mason_lspconfig = require 'mason-lspconfig'
---
--- mason_lspconfig.setup {
---   ensure_installed = vim.tbl_keys(servers),
--- }
---
--- mason_lspconfig.setup_handlers {
---   function(server_name)
---     require('lspconfig')[server_name].setup {
---       capabilities = capabilities,
---       on_attach = on_attach,
---       settings = servers[server_name],
---       filetypes = (servers[server_name] or {}).filetypes,
---     }
---   end
--- }
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
