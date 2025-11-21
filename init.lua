b4c2e5807c4c4ea92bc54ca5c1f9c7a4049f63c7
FlanGrande <francisjgrande@gmail.com>
2025-11-21 15:14:50 +0000

    New keymap to run Godot with colliders debugging

--- init.lua
+++ init.lua
@@ -893,12 +893,15 @@ local GodotNimTerminal  = require('toggleterm.terminal').Terminal
 local root = vim.fs.root(0, '.git')
 local godotbuildgame = GodotNimTerminal:new({ cmd = "gdextwiz build", dir = root, hidden = true, close_on_exit = false })
 local godotrungame = GodotNimTerminal:new({ cmd = "godot godot/main.tscn", dir = root, hidden = true, close_on_exit = false })
-
+local godotrungamecolliders = GodotNimTerminal:new({ cmd = "godot godot/main.tscn --debug-collisions" , dir = root, hidden = true, close_on_exit = false })
+  
 function _build_godot_game() godotbuildgame:toggle() end
 function _run_godot_game() godotrungame:toggle() end
+function _run_godot_game_with_colliders() godotrungamecolliders:toggle() end
 
 vim.keymap.set('n', '<leader>nb', '<cmd>lua _build_godot_game()<CR>', { noremap = true, silent = true, desc = "Godot-[N]im [B]uild game" })
 vim.keymap.set('n', '<leader>ng', '<cmd>lua _run_godot_game()<CR>', { noremap = true, silent = true, desc = "Godot-[N]im run [G]ame" })
+vim.keymap.set('n', '<leader>nc', '<cmd>lua _run_godot_game_with_colliders()<CR>', { noremap = true, silent = true, desc = "Godot-[N]im run game with [C]olliders" })
 
 
 -- End Mine
